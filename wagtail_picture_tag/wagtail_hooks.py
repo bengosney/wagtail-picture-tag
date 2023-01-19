016183e70e216f06882365eb7b9f12aab7018dc5
Ben Gosney <bengosney@googlemail.com>
2023-01-19 16:40:23 +0000

    WIP: try and hook into wagtail better

--- wagtail_picture_tag/wagtail_hooks.py
+++ wagtail_picture_tag/wagtail_hooks.py
@@ -1,5 +1,16 @@
 from wagtail import hooks
 from wagtail.images import image_operations
+from wagtail.images.models import IMAGE_FORMAT_EXTENSIONS
+import contextlib
+
+
+#with contextlib.suppress(ImportError):
+    # Third Party
+import willowavif  # noqa
+from willowavif import AvifPlugin  # noqa
+
+
+IMAGE_FORMAT_EXTENSIONS['avif'] = '.avif'
 
 
 class FormatOperation(image_operations.FormatOperation):
@@ -7,11 +18,11 @@ class FormatOperation(image_operations.FormatOperation):
         self.format = format
         self.options = options
 
-        if self.format not in ["jpeg", "png", "gif", "webp", "avif"]:
-            raise ValueError("Format must be either 'jpeg', 'png', 'gif', 'webp' or 'avif'")
+        if self.format not in IMAGE_FORMAT_EXTENSIONS.keys():
+            raise ValueError(f"Format must be one of: {', '.join(IMAGE_FORMAT_EXTENSIONS.keys())}")
 
 
-@hooks.register("register_image_operations")
+@hooks.register("register_image_operations", order=1)
 def register_image_operations():
     return [
         ("format", FormatOperation),
